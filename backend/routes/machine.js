9755ef158cfe4c970197fc5d381da7bb68ccddd0
Aravindh-Subramaniyan <aravindhsubramaniyan2001@gmail.com>
2021-11-15 16:18:20 +0530

    Final Changes

--- backend/routes/machine.js
+++ backend/routes/machine.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const machineJs = require("../models/machine");
-// const Machine = require("../models/machine");
-
+var app = require("../app");
+var FileName;
+var Machine;
 const machineSchema = new mongoose.Schema({}, { strict: false });
-const Machine = mongoose.model("Dynamic", machineSchema);
+var usercollection, keyValue;
+//Machine = mongoose.model("JH check sheet (1).csv", machineSchema);
+//import * as app from "./app.js";
 
+// appFile = new app();
 const router = express.Router();
+mongoose.connection.on("open", (ref) => {
+  mongoose.connection.db.listCollections().toArray((err, collections) => {
+    collectionvals = collections.map((collection) => {
+      return collection.name;
+    });
+    var colldata = collectionvals.filter((data) => !data.includes("_keyvals"));
+    if (colldata) {
+      usercollection = colldata[0];
+      // var keyval = usercollection.slice(0, usercollection.length - 1);
+      // var keyvalvar = keyval + "_keyvals";
+      // keyValue = collectionvals.some((data) => data.includes(keyvalvar));
+      // console.log("keyval", keyvalvar);
+      // console.log("KeyValue", keyValue);
+    } else {
+      console.log("No collections");
+    }
+  });
+});
+
+//const collectionvals = app.passcollections();
 
 router.get("/", (req, res, next) => {
+  if (usercollection == undefined) {
+    return;
+  }
+  Machine = mongoose.model(usercollection, machineSchema);
   Machine.find()
     .select({ _id: 0, __v: 0 })
     .then((documents) => {
-      // console.log(documents);
       res.status(200).json({
         message: "Machines fetched successfully!",
         machines: documents,
@@ -20,7 +47,53 @@ router.get("/", (req, res, next) => {
     });
 });
 
+router.get("/machinekeyvalues", (req, res, next) => {
+  if (usercollection == undefined) {
+    return;
+  }
+  var keyvalcollection = usercollection.slice(0, usercollection.length - 1);
+  Machine = mongoose.model(keyvalcollection + "_keyvals", machineSchema);
+  Machine.find()
+    .select({ _id: 0, __v: 0 })
+    .then((documents) => {
+      // console.log("MachineKey value function executed");
+      res.status(200).json({
+        message: "KeyValues fetched successfully!",
+        machines: documents,
+      });
+    });
+});
+
+router.post("/usercollection", (req, res) => {
+  let data = req.body.collection;
+  if (data) {
+    usercollection = data;
+    res
+      .status(201)
+      .json({ message: "User Collection received in the backend" });
+  }
+});
+
+router.get("/collections", (req, res) => {
+  //var collectionvals = [];
+  mongoose.connection.db.listCollections().toArray((err, collections) => {
+    collectionvals = collections.map((collection) => {
+      return collection.name;
+    });
+    var colldata = collectionvals.filter((data) => !data.includes("_keyvals"));
+    if (colldata) {
+      console.log("Colldata", colldata);
+      usercollection = colldata[0];
+    } else {
+      console.log("No collections");
+    }
+  });
+  console.log("Collections in router: ", collectionvals);
+  res.status(200).json(collectionvals);
+});
+
 router.get("/:id", (req, res, next) => {
+  Machine = mongoose.model("jh check sheet (1).csvs", machineSchema);
   Machine.findById(req.params.id).then((machine) => {
     if (machine) {
       res.status(200).json(machine);
@@ -42,8 +115,10 @@ router.post("/", (req, res, next) => {
 });
 
 router.post("/import", (req, res) => {
-  const data = req.body;
-  Machine.insertMany(data, (err, data) => {
+  FileName = req.body.Name;
+  const data = req.body.Data;
+  MachineData = mongoose.model(FileName, machineSchema);
+  MachineData.insertMany(data, (err, data) => {
     if (err) {
       res.status(400).json({
         message: "There is some error to Uploading CSV!",
@@ -53,6 +128,23 @@ router.post("/import", (req, res) => {
         message: "Filtered File Uploaded Successfully!",
       });
     }
+  });
+});
+
+// function callback() {
+//   const machineSchema = new mongoose.Schema({}, { strict: false });
+//   Machine = mongoose.model(FileName + "_KeyValue", machineSchema);
+// }
+
+router.post("/keyvalues", (req, res) => {
+  FileName = req.body.Name;
+  console.log("File Name", FileName);
+  MachineKeyVal = mongoose.model(FileName + "_KeyVal", machineSchema);
+  const data = new MachineKeyVal(req.body.Data);
+  data.save().then(() => {
+    res.status(201).json({
+      message: "Key-Value Pairs added succesfully!",
+    });
   });
 });
 
